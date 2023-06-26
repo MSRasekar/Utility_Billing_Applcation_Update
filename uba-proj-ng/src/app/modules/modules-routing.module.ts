1d6db23e8a86f791c5abc29a88045ea816b9430d
MSRasekar <msraekar@gmail.com>
2023-06-26 17:39:42 +0530

    various modules added like admin user

---
+++ uba-proj-ng/src/app/modules/modules-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { StepsComponent } from './components/steps/steps.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'signup', component:SignupComponent},
+  {path: 'steps', component:StepsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ModulesRoutingModule { }

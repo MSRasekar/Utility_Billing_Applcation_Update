d898c6bcaa753e25982b3718ebf600870d1272cd
MSRasekar <msraekar@gmail.com>
2023-07-01 14:13:35 +0530

    different components added like home about etc

---
+++ UBA-Proj-Phase-2/src/app/modules/components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  constructor(public loginService: LoginService) {}
+
+  logout(): void {
+
+    this.loginService.logout();
+    // Remove user id from session storage
+    sessionStorage.removeItem('userid');
+  }
+}

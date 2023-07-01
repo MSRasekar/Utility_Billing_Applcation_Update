3d9ff5cd2e86a7f8268c9d58075d825308882661
MSRasekar <msraekar@gmail.com>
2023-07-01 21:02:51 +0530

    user module and user routing added

---
+++ UBA-Proj-Phase-2/src/app/modules/user/user.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TrackStatusComponent } from './track-status/track-status.component';
+import { UserRoutingModule } from './user-routing.module';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MeterDetailsComponent } from './meter-details/meter-details.component';
+import { UserSideBarComponent } from './user-side-bar/user-side-bar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import { RaiseComplaintComponent } from './raise-complaint/raise-complaint.component';
+
+
+@NgModule({
+  declarations: [
+    TrackStatusComponent,
+    UserProfileComponent,
+    MeterDetailsComponent,
+    UserSideBarComponent,
+    RaiseComplaintComponent
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ]
+})
+export class UserModule { }

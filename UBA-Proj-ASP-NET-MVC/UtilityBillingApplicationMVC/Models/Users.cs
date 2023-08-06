fc7ede9d0f524e4209fa52deb2ba9d640f959eef
MSRasekar <msraekar@gmail.com>
2023-08-07 01:01:46 +0530

    All Model classes added

---
+++ UBA-Proj-ASP-NET-MVC/UtilityBillingApplicationMVC/Models/Users.cs
@@ -0,0 +1,33 @@
+﻿
+namespace UtilityBillingApplicationMVC.Models;
+
+public partial class Users
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+
+    public string Email { get; set; }
+
+    public string Gender { get; set; }
+
+    public DateTime Dob { get; set; }
+
+    public string MaritalStatus { get; set; }
+
+    public virtual ICollection<Addresses> Addresses { get; set; } = new List<Addresses>();
+
+    public virtual ICollection<ApplicationStatuses> ApplicationStatuses { get; set; } = new List<ApplicationStatuses>();
+
+    public virtual ICollection<BillHistories> BillHistories { get; set; } = new List<BillHistories>();
+
+    public virtual ICollection<MeterInfos> MeterInfos { get; set; } = new List<MeterInfos>();
+
+    public virtual ICollection<Payments> Payments { get; set; } = new List<Payments>();
+
+    public virtual ICollection<Tickets> Tickets { get; set; } = new List<Tickets>();
+
+    public virtual UsersRoles UsersRoles { get; set; }
+}

df153b6fc5f650d691d9d52c29b5f623dacd431e
MSRasekar <msraekar@gmail.com>
2023-08-08 18:08:08 +0530

    update user profile input model

---
+++ UBA-Proj-ASP-NET-MVC/UtilityBillingApplicationMVC/DTOs/UpdateUserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace UtilityBillingApplicationMVC.DTOs
+{
+    public class UpdateUserModel
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public DateTime Dob { get; set; }
+        public string MaritalStatus { get; set; }
+    }
+}

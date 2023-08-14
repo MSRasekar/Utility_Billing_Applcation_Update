65301b8f340cf574a287e0ce4ca779141ed9dee8
MSRasekar <msraekar@gmail.com>
2023-08-14 18:50:59 +0530

    Application User and Db Context added

---
+++ UBA-Proj-ASP-NET-API-Angular/UtilityBillingApplicationAPI/Entites/Models/ApplicationUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace UtilityBillingApplicationAPI.Entites.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        [Required(ErrorMessage = " Gender is required")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = " Dob is required")]
+        public DateTime Dob { get; set; }
+        [Required(ErrorMessage = " MaritalStatus is required")]
+        public string MaritalStatus { get; set; }
+
+        [Required(ErrorMessage = " Address1 is required")]
+        public string AddressLine { get; set; }
+        [Required(ErrorMessage = " City is required")]
+        public string City { get; set; }
+        [Required(ErrorMessage = " State is required")]
+        public string State { get; set; }
+        [Required(ErrorMessage = " Pincode is required")]
+        public string Pincode { get; set; }
+
+
+
+
+        public virtual ICollection<ApplicationStatus> ApplicationStatus { get; set; }
+
+        public virtual Meter Meter { get; set; }
+
+        public virtual ICollection<Payments> Payments { get; set; }
+
+        public virtual ICollection<BillingHistory> BillingHistory { get; set; }
+        public virtual ICollection<Ticket> Ticket { get; set; }
+
+
+    }
+}
---
+++ UBA-Proj-ASP-NET-API-Angular/UtilityBillingApplicationAPI/Entites/Models/UtilityBillingContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace UtilityBillingApplicationAPI.Entites.Models
+{
+    public class UtilityBillingContext : IdentityDbContext<ApplicationUser>
+    {
+        public UtilityBillingContext(DbContextOptions<UtilityBillingContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedRoles(builder);
+
+            builder.Entity<Meter>()
+                .HasOne(m => m.User)
+                .WithOne(u => u.Meter)
+                .HasForeignKey<Meter>(m => m.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<BillingHistory>()
+                .HasOne(bh => bh.Meter)
+                .WithMany(m => m.BillingHistory)
+                .HasForeignKey(bh => bh.MeterId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<BillingHistory>()
+                .HasOne(b => b.User)
+                .WithMany(u => u.BillingHistory)
+                .HasForeignKey(b => b.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<BillingHistory>()
+                .Property(bh => bh.MeterReading)
+                .HasPrecision(18, 2);
+
+            builder.Entity<BillingHistory>()
+                .Property(bh => bh.BillingAmount)
+                .HasPrecision(18, 2);
+
+            builder.Entity<Payments>()
+                .Property(p => p.PaymentAmount)
+                .HasPrecision(18, 2);
+        }
+
+        private static void SeedRoles(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityRole>().HasData
+            (
+                new IdentityRole() { Name = "SuperAdmin", ConcurrencyStamp = "1", NormalizedName = "SuperAdmin" },
+                new IdentityRole() { Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },
+                new IdentityRole() { Name = "User", ConcurrencyStamp = "2", NormalizedName = "User" }
+            );
+        }
+
+        public virtual DbSet<Meter> Meters { get; set; }
+        public virtual DbSet<ApplicationStatus> ApplicationStatus { get; set; }
+        public virtual DbSet<BillingHistory> BillingHistory { get; set; }
+        public virtual DbSet<Payments> Payments { get; set; }
+        public virtual DbSet<Ticket> Tickets { get; set; }
+    }
+}

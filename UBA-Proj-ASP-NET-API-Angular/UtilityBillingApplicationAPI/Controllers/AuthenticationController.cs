9a933a8f6bb2159e19c70ee1a7de4e627e8b98ea
MSRasekar <msraekar@gmail.com>
2023-08-14 18:54:34 +0530

    Auth Controller added with Register and login endpoint

---
+++ UBA-Proj-ASP-NET-API-Angular/UtilityBillingApplicationAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UtilityBillingApplicationAPI.Entites.DTOs.Authentication;
+using UtilityBillingApplicationAPI.Entites.DTOs;
+using UtilityBillingApplicationAPI.Entites.Models;
+using UtilityBillingApplicationAPI.Repositories;
+
+namespace UtilityBillingApplicationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IAuthRepository _authRepository;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager, IAuthRepository authRepository)
+        {
+            _userManager = userManager;
+            _authRepository = authRepository;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationDTO registerUser, string role)
+        {
+            var userExist = await _userManager.FindByEmailAsync(registerUser.Email);
+
+            if (userExist != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, new ResponseDTO { Status = "Error", Message = "User already exists!" });
+            }
+
+            var result = await _authRepository.RegisterUserAsync(registerUser, role);
+
+            if (result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status201Created, new ResponseDTO { Status = "Success", Message = "User created Successfully!" });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDTO { Status = "Error", Message = "User registration failed!" });
+            }
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginUser)
+        {
+            var result = await _authRepository.LoginUserAsync(loginUser);
+
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByEmailAsync(loginUser.Username);
+                var token = await _authRepository.GenerateJwtToken(user);
+
+                return Ok(new AuthResponseDTO { IsAuthSuccessful = true, Token = token });
+            }
+            else
+            {
+                return Unauthorized(new AuthResponseDTO { ErrorMessage = "Invalid Authentication" });
+            }
+        }
+    }
+}
